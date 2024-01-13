828512f251c54d8b31b896381b2e76fdc496f33e
Onur Baykal <theaob@gmail.com>
2024-01-13 14:19:34 +0300

    add game logic on collide

--- level_1.gd
+++ level_1.gd
@@ -1,4 +1,10 @@
 extends Node2D
 
 func _ready():
-	print("Hello world!")
+	print("Starting Level 1")
+
+
+func _on_maze_body_exited(_body):
+	print("Body exited. Reloading scene...")
+	get_tree().reload_current_scene()
+	pass # Replace with function body.
--- player.gd
+++ player.gd
@@ -1,19 +1,19 @@
 extends RigidBody2D
-	
+    
 const MOVEMENT_SPEED = 1000
-	
+    
 func _physics_process(_delta):
-	var upPressed = Input.is_action_pressed("move_up")
-	var downPressed = Input.is_action_pressed("move_down")
-	var leftPressed = Input.is_action_pressed("move_left")
-	var rightPressed = Input.is_action_pressed("move_right")
-	
-	var yForce = 0
-	var xForce = 0
-	
-	if upPressed: yForce += -MOVEMENT_SPEED
-	if downPressed: yForce += MOVEMENT_SPEED
-	if leftPressed: xForce += -MOVEMENT_SPEED
-	if rightPressed: xForce += MOVEMENT_SPEED
-		
-	apply_force(Vector2(xForce, yForce))
+    var upPressed = Input.is_action_pressed("move_up")
+    var downPressed = Input.is_action_pressed("move_down")
+    var leftPressed = Input.is_action_pressed("move_left")
+    var rightPressed = Input.is_action_pressed("move_right")
+    
+    var yForce = 0
+    var xForce = 0
+    
+    if upPressed: yForce += -MOVEMENT_SPEED
+    if downPressed: yForce += MOVEMENT_SPEED
+    if leftPressed: xForce += -MOVEMENT_SPEED
+    if rightPressed: xForce += MOVEMENT_SPEED
+        
+    apply_force(Vector2(xForce, yForce))
